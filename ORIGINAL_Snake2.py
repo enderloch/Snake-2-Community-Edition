88f00b8b637f255509edabbe079590e4f7ee6a10
Jimmy5542 <85048674+Jimmy5542@users.noreply.github.com>
2023-07-06 23:07:45 +0700

    Revert ORIGINAL_Snake2.py back to original state
    Reverted the file back into it's original state in case it needs to be changed.

--- ORIGINAL_Snake2.py
+++ ORIGINAL_Snake2.py
@@ -5,13 +5,9 @@ import random
 import microtransactions  # Import the microtransactions module
 import time
 import random
-import os
 
 # Determine if the DLC is activated
 DLC_ACTIVATED = random.random() < 0.25  # 20% chance of DLC being activated
-
-# Get current path directory of script
-script_path = os.path.dirname(__file__)
 
 # Initialize Pygame
 pygame.init()
@@ -23,7 +19,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Snake RPG Game")
 
 # Load background image
-background_img = pygame.image.load(os.path.join(script_path, "images", "American Flag.png"))
+background_img = pygame.image.load("American Flag.png")
 background_img = pygame.transform.scale(background_img, (screen_width, screen_height))
 
 # Set colors
@@ -31,7 +27,7 @@ snake_color = (0, 255, 0)
 food_color = (0, 255, 0)
 
 # Load Guy Fieri image for the snake
-snake_img = pygame.image.load(os.path.join(script_path, "images", "guy_fieri.png"))
+snake_img = pygame.image.load("guy_fieri.png")
 snake_img = pygame.transform.scale(snake_img, (40, 40))  # Increase size to 40x40 pixels
 
 # Define snake properties
@@ -62,10 +58,10 @@ score = 0
 font = pygame.font.Font(None, 36)
 
 # Load game over sound
-game_over_sound = pygame.mixer.Sound(os.path.join(script_path, "audio", "Snake Snake Snake.wav"))
+game_over_sound = pygame.mixer.Sound("Snake Snake Snake.wav")
 
 # Load level up sound
-level_up_sound = pygame.mixer.Sound(os.path.join(script_path, "audio", "Minecraft Level Up.mp3"))
+level_up_sound = pygame.mixer.Sound("Minecraft Level Up.mp3")
 
 # Define snake body
 snake_body = []
@@ -240,8 +236,8 @@ while not game_over:
         microtransactions.show_microtransaction_window(screen)
 
 # Define the credits
-credits_font_normal = pygame.font.Font(os.path.join(script_path, "fonts" ,"Roboto-Italic.ttf"), 36)
-credits_font_bold = pygame.font.Font(os.path.join(script_path, "fonts", "Roboto-Bold.ttf"), 36)
+credits_font_normal = pygame.font.Font("Roboto-Italic.ttf", 36)
+credits_font_bold = pygame.font.Font("Roboto-Bold.ttf", 36)
 credits_lines = [
     {"text": "SNAKE 2", "font": credits_font_bold},
     {"text": "Powered by Twitch Chat", "font": credits_font_normal},
