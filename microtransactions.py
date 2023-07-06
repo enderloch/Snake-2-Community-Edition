64faf602d006e8a3679a374e93b5dd093092ff38
Enderloch <138721842+enderloch@users.noreply.github.com>
2023-07-05 20:23:45 -0400

    Patch
    Made it so it redirects files to folders, and organized file system

---
+++ microtransactions.py
@@ -0,0 +1,64 @@
+# microtransactions.py
+
+import random
+import os
+import pygame
+
+# Get the current script's file path
+current_path = os.path.dirname(__file__)
+
+# Specify the folder name where your MP3 files are located
+audio_folder = os.path.join(current_path, 'audio')
+
+# Initialize Pygame
+pygame.init()
+
+# Define colors
+bg_color = (0, 0, 0)
+
+# Define font
+font = pygame.font.Font(None, 36)
+
+# Define microtransaction items and their prices
+items = {
+    "Super Speed": 5,
+    "Extra XP": 10,
+    "Mega Growth": 8,
+    "Bonus Life": 15,
+    "Glob": 20
+}
+
+
+# Load MP3 files for microtransactions
+microtransaction_files = [
+    "Buying Bonus Life.mp3",
+    "Buying Extra XP.mp3",
+    "Buying Mega Growth.mp3",
+    "Buying Super Speed.mp3",
+    "glob.mp3"
+]
+
+microtransaction_sounds = []
+for filename in microtransaction_files:
+    file_path = os.path.join(audio_folder, filename)
+    sound = pygame.mixer.Sound(file_path)
+    microtransaction_sounds.append(sound)
+
+# Function to play a random microtransaction sound
+def play_random_microtransaction_sound():
+    sound_file = random.choice(microtransaction_files)
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
+
+# Function to show microtransaction window
+def show_microtransaction_window(screen):
+    pygame.draw.rect(screen, (128, 128, 128), (200, 200, 600, 400))
+    pygame.draw.rect(screen, (0, 0, 0), (220, 220, 560, 360))
+    title_text = font.render("Microtransactions", True, (255, 255, 255))
+    screen.blit(title_text, (300, 220))
+    y = 270
+    for item, price in items.items():
+        item_text = font.render(f"{item} - Price: ${price}", True, (255, 255, 255))
+        screen.blit(item_text, (250, y))
+        y += 40
+    pygame.display.flip()
